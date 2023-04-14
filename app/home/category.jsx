efb11bc95d034b96b9f1c859abdafdd9f78c822a
Olawoyin Daniel <olawoyindaniel95@gmail.com>
2023-04-14 18:15:29 +0100

    finished the category screen

--- app/home/category.jsx
+++ app/home/category.jsx
@@ -1,9 +1,14 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Tabs } from "expo-router";
 import { SimpleLineIcons } from "@expo/vector-icons";
+import { HomeLayout } from "../../Components";
+import { topics } from "../../lib/constants";
+import Topic from "../../Components/Topic";
 
 const category = () => {
+    const [selected, setSelected] = useState([]);
+
     return (
         <>
             <Tabs.Screen
@@ -13,6 +18,31 @@ const category = () => {
                     ),
                 }}
             />
+            <HomeLayout
+                heading={"Categories"}
+                subheading={"Thousands of articles in each category"}
+            >
+                <View className="flex-1 mt-[32] flex-row flex-wrap  items-center justify-evenly">
+                    {topics.map((topic, id) => (
+                        <Topic
+                            title={topic.title}
+                            selected={selected}
+                            key={`cate_${id}`}
+                            handlePress={() => {
+                                if (selected.includes(topic.title)) {
+                                    setSelected(
+                                        selected.filter(
+                                            (item) => item !== topic.title
+                                        )
+                                    );
+                                } else {
+                                    setSelected([...selected, topic.title]);
+                                }
+                            }}
+                        />
+                    ))}
+                </View>
+            </HomeLayout>
         </>
     );
 };
